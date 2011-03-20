18dd5dd3a05d320d70186f02e8210752f3cb414a
wtaylor <wtaylor@d7911c9c-ba0c-4a86-bf38-f0db2918b201>
2011-03-20 03:31:18 +0000

    setting email to handle utf-8.

--- src/public_html/EmailUtil.php
+++ src/public_html/EmailUtil.php
@@ -3,7 +3,15 @@
 class EmailUtil
 {
 	public static function send($m) {
-		mail(
+		// set character encoding to UTF-8.
+		mb_language('uni');
+		mb_internal_encoding('UTF-8');
+		
+		// this encodes the subject and the message body.
+		//
+		// this does not encode email names. For example, the 'John Smith' in 
+		// the following is the name: "John Smith <john.smith@business.com>)".
+		mb_send_mail(
 			$m['to'],
 			$m['subject'],
 			wordwrap($m['text'], 70),
@@ -11,7 +19,7 @@ class EmailUtil
 			"Reply-To: {$m['from']}".PHP_EOL.
 			"Bcc: {$m['bcc']}".PHP_EOL.
 			'MIME-Version: 1.0'.PHP_EOL.
-			'Content-type: text/html; charset=iso-8859-1'.PHP_EOL
+			'Content-Type: text/html; charset=utf-8'.PHP_EOL
 		);
 	}
 }
