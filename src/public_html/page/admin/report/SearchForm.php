030204eaf6e109ca118328ab7e733cd6b6b4a05c
wtaylor <wtaylor@d7911c9c-ba0c-4a86-bf38-f0db2918b201>
2011-04-01 01:58:55 +0000

    changed text/layout.

--- src/public_html/page/admin/report/SearchForm.php
+++ src/public_html/page/admin/report/SearchForm.php
@@ -1,6 +1,9 @@
 
 <tr>
-	<td colspan="2">
+	<td class="label">
+		Search for
+	</td>
+	<td>
 		<?php echo $this->HTML->hidden(array(
 			'name' => 'reportId',
 			'value' => $this->info['reportId']
@@ -11,12 +14,18 @@
 			'value' => $this->info['eventId']
 		)) ?>
 		
-		Look for
 		<?php echo $this->HTML->text(array(
 			'name' => 'term',
-			'value' => ''
+			'value' => '',
+			'size' => 20
 		)) ?>
-		in
+	</td>
+</tr>
+<tr>
+	<td class="label">
+		Column
+	</td>
+	<td>
 		<?php echo fragment_reportField_HTML::select($this->info['event'], false) ?>
 	</td>
 </tr>
