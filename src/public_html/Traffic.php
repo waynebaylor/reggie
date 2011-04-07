8aed6ae9f8bf007498fcd68e629e67dbed773f68
wtaylor <wtaylor@d7911c9c-ba0c-4a86-bf38-f0db2918b201>
2011-04-07 03:20:39 +0000

    changed front controller so only implmentation is uri -> class, but added support for dir prefixes. also removed the reverse method.

--- src/public_html/Traffic.php
+++ src/public_html/Traffic.php
@@ -1,69 +1,88 @@
 <?php
 
 /**
- * Interface for Traffic controllers.
+ * Front controller for routing URIs. 
+ *
  * @author wtaylor
  *
  */
-interface TrafficController
+class Traffic
 {
-	/**
-	 * Process the given URI. 
-	 * @param string $uri the cleaned-up URI 
-	 * @param array $match any regular expression matches
-	 */
-	public function handle($uri, $matches);
+	private static $separator = 'q';
+	private static $context = '/';
+	private static $prefixes = array();
 	
 	/**
-	 * Convert the given URI info into a URI.
-	 * @param string $context the context root/path
-	 * @param string $path the URI path
-	 * @param string $params named parameters
+	 * Set the context path, default is '/'. This is used in routing/reversing URIs.
+	 * @param string $c the context root/path
 	 */
-	public function reverse($context, $path, $params);
-}
-
-/**
- * Controller that maps URIs to classes and invokes an action method. The URI
- * contains the path to the class, the action method, and any parameter values.
- * For example:
- * <ul>
- *  <li> '/path/to/class/exec/q/val1/val2' is mapped to: path_to_Class->exec(val1, val2)</li>
- * </ul>  
- * 
- * @author wtaylor
- *
- */
-class ConventionalController implements TrafficController
-{
-	public function __construct($dirPrefix = '', $separator = 'q') {
-		$this->prefix = $dirPrefix;
-		$this->separator = $separator;
+	public static function context($c) {
+		self::$context = trim($c, '/');
 	}
 	
-	public function handle($uri, $matches) {
-		$info = $this->parseUri($uri);
+	/**
+	 * A mapping of URI prefix to directory. This is used when the URI doesn't have the
+	 * complete class name. 
+	 * @param array $p the prefixes
+	 */
+	public static function prefixes($pref) {
+		foreach ($pref as $p => $d) {
+			$p = trim($p, '/');
+			if(!empty($p)) {
+				self::$prefixes[$p] = $d;
+			}
+		}
+	}
+	
+	/**
+	 * Set the separator to use between action segment and param segments, default is 'q'.
+	 * @param unknown_type $s the segment char(s)
+	 */
+	public static function separator($s) {
+		if(!empty($s)) {
+			self::$separator = $s;
+		}
+	}
+	
+	/**
+	 * Direct the given request URI to the matching class and action method.
+	 * @param string $requestUri the request URI
+	 */
+	public static function route($uri) {
+		// convert URI to class, action, and params.
+		$info = self::parseUri($uri, self::$prefixes, self::$separator);
 		
+		// invoke action method with params as arguments.
 		$obj = new $info['className'];
 		call_user_func_array(array($obj, $info['action']), $info['params']);
 	}
 	
-	public function reverse($context, $path, $params) {
+	/**
+	 * Converts the given URI to a class name, action method, and params.
+	 * @param string $uri the URI
+	 * @param array $prefixes array of URI segment -> dir 
+	 * @param string $separator chars to use when determining which segments are params
+	 * @throws Exception if URI cannot be converted
+	 */
+	public static function parseUri($uri, $prefixes, $separator) {
+		$uri = trim($uri, '/');
 		
-	}
-	
-	public function parseUri($uri) {
+		// remove any fragment/anchor and query parameters.
+		$uri = preg_replace('/#.*+$/', '', $uri);
+		$uri = preg_replace('/\?.*+$/', '', $uri);
+		
+		// remove context path.
+		$uri = preg_replace('/^'.self::$context.'\/?/', '', $uri);
+		
 		$segments = explode('/', $uri);
 		
+		// check if we have at least a class and action method.
 		if(count($segments) < 2) {
 			throw new Exception('Error parsing URI: "'.$uri.'". Must contain at least two segments');
 		}
 		
-		if(!empty($this->prefix)) {
-			array_unshift($segments, $this->prefix);
-		}
-		
-		$index = array_search($this->separator, $segments);
+		// break uri into pieces and pick out the class name part, action, and params.
+		$index = array_search($separator, $segments);
 		
 		if($index !== false) {
 			$classSegments = array_slice($segments, 0, $index-1);
@@ -80,99 +99,34 @@ class ConventionalController implements TrafficController
 		// upper-case the last segment in the class name.
 		$classSegments[] = ucfirst(array_pop($classSegments));
 		
+		// break prefix dir into pieces so we can include in class name.
+		$dir = self::getDirPrefix($uri, $prefixes);
+		$prefixSegments = empty($dir)? array() : explode('/', $dir);
+		
+		// combine prefix with URI segments.
+		$classSegments = array_merge($prefixSegments, $classSegments);
+		
 		return array(
 			'className' => implode('_', $classSegments),
 			'action' => $actionMethod[0],
 			'params' => $paramSegments
 		);
 	}
-}
-
-////////////////////////////////////////////////////////////////
-
-/**
- * Front controller for routing URIs. 
- *
- * @author wtaylor
- *
- */
-class Traffic
-{
-	private static $context = '/';
-	private static $routes = array();
 	
 	/**
-	 * Set the context path, default is '/'. This is used in routing/reversing URIs.
-	 * @param string $c the context root/path
+	 * Checks the given URI agains the prefixes defined. If a prefix matches
+	 * the beginning of he URI, then the corresponding dir is returned. If no
+	 * match is found then an empty string is returned.
+	 * @param string $uri
 	 */
-	public static function context($c) {
-		self::$context = trim($c, '/');
-	}
-	
-	/**
-	 * Assign a controller to handle URIs that match the given pattern. The pattern can
-	 * be a regular expression if you follow the two rules:
-	 * <ul>
-	 * <li>Omit the surrounding delimiters. '/the(.*)pattern/' should be written as 'the(.*)pattern'.</li>
-	 * <li>Omit the start/end anchors: '^/some/path/\d{3}$' should be written as '/some/path/\d{3}'.</li>
-	 * <li>Do not escape forward slashes: '/this/pattern/(\d+)/is/okay'.</li>
-	 * @param string $pattern the URI pattern to match
-	 * @param TrafficController $controller the instance that will handle matching requests
-	 */
-	public static function route($pattern, $controller) {
-		if(!empty($pattern) && isset($controller)) {
-			$pattern = trim($pattern, '/');
-			$segments = explode('/', $pattern);
-			
-			$r = new stdClass();
-			$r->regex = '/^'.implode('\/', $segments).'/';
-			$r->controller = $controller;
-			
-			self::$routes[] = $r;
-		}
-	}
-	
-	/**
-	 * Direct the given request URI to the matching controller for processing.
-	 * @param string $requestUri the request URI
-	 * @throws Exception if no match is made
-	 */
-	public static function direct($requestUri) {
-		$uri = trim($requestUri, '/');
-		
-		// remove any fragment/anchor and query parameters.
-		$uri = preg_replace('/#.*+$/', '', $uri);
-		$uri = preg_replace('/\?.*+$', '', $uri);
-		
-		// remove context path.
-		$uri = preg_replace('/^'.self::$context.'\/?/', '', $uri);
-		
-		foreach(self::$routes as $route) {
-			$matches = array();
-			$isMatch = preg_match($route->regex, $uri, $matches);
-			if($isMatch > 0) {
-				$route->controller->handle($uri, $matches);
-				return;
+	public static function getDirPrefix($uri, $prefixes) {
+		foreach($prefixes as $p => $d) {
+			if(preg_match('/^'.$p.'/', $uri) > 0) {
+				return $d;
 			}
 		}
 		
-		throw Exception('No route found for URI: '.$requestUri);
-	}
-	
-	/**
-	 * Convert the given path and parameters into a URI. 
-	 * @param string $path the path
-	 * @param array $params the parameters
-	 * @throws Exception if a match is not made
-	 */
-	public static function reverse($path, $params) {
-		foreach(self::$routes as $route) {
-			if(preg_match($route->regex, $path)) {
-				return $route->controller->reverse(self::$context, $path, $params);
-			}
-		}
-		
-		throw new Exception('No route found for link path: '.$path);
+		return '';
 	}
 }
 
--- src/test/TrafficTest.php
+++ src/test/TrafficTest.php
@@ -6,116 +6,12 @@ class ConventionalControllerTest extends PHPUnit_Framework_TestCase
 		Traffic::context('/');
 	}
 	
-	public function testParseUriNoArgsNoParams() {
-		$c = new ConventionalController();
-		$result = $c->parseUri('hello/world');
+	public function testParseUriNoPrefixesDefaultSegment() {
+		$result = Traffic::parseUri('hello/world', array(), 'q');
 		
 		$this->assertEquals($result['className'], 'Hello');
 		$this->assertEquals($result['action'], 'world');
 		$this->assertEmpty($result['params']);
-	}
-	
-	public function testParseUriNoArgsWithSeparator() {
-		$c = new ConventionalController();
-		$result = $c->parseUri('hello/world/q');
-		
-		$this->assertEquals($result['className'], 'Hello');
-		$this->assertEquals($result['action'], 'world');
-		$this->assertEmpty($result['params']);
-	}
-	
-	public function testParseUriNoArgsWithParams() {
-		$c = new ConventionalController();
-		$result = $c->parseUri('hello/world/q/value/7');
-		
-		$this->assertEquals($result['className'], 'Hello');
-		$this->assertEquals($result['action'], 'world');
-		$this->assertEquals($result['params'][0], 'value');
-		$this->assertEquals($result['params'][1], '7');
-	}
-	
-	public function testParseUriWithDirNoParams() {
-		$c = new ConventionalController('dir');
-		$result = $c->parseUri('hello/world');
-		
-		$this->assertEquals($result['className'], 'dir_Hello');
-		$this->assertEquals($result['action'], 'world');
-		$this->assertEmpty($result['params']);
-	}
-	
-	public function testParseUriWithDirWithParamSeparator() {
-		$c = new ConventionalController('dir');
-		$result = $c->parseUri('hello/world/q');
-		
-		$this->assertEquals($result['className'], 'dir_Hello');
-		$this->assertEquals($result['action'], 'world');
-		$this->assertEmpty($result['params']);
-	}
-	
-	public function testParseUriWithDirWithParams() {
-		$c = new ConventionalController('dir');
-		$result = $c->parseUri('hello/world/q/value/7');
-		
-		$this->assertEquals($result['className'], 'dir_Hello');
-		$this->assertEquals($result['action'], 'world');
-		$this->assertEquals($result['params'][0], 'value');
-		$this->assertEquals($result['params'][1], '7');
-	}
-	
-	public function testParseUriNoParamsWithEmptyDirAndSeparator() {
-		$c = new ConventionalController('', 'qw');
-		$result = $c->parseUri('hello/world');
-		
-		$this->assertEquals($result['className'], 'Hello');
-		$this->assertEquals($result['action'], 'world');
-		$this->assertEmpty($result['params']);
-	}
-	
-	public function testParseUriWithSeparatorEmptyDir() {
-		$c = new ConventionalController('', 'qw');
-		$result = $c->parseUri('hello/world/qw');
-		
-		$this->assertEquals($result['className'], 'Hello');
-		$this->assertEquals($result['action'], 'world');
-		$this->assertEmpty($result['params']);
-	}
-	
-	public function testParseUriWithEmptyDirSeparatorAndParams() {
-		$c = new ConventionalController('', 'qw');
-		$result = $c->parseUri('hello/world/qw/value/7');
-		
-		$this->assertEquals($result['className'], 'Hello');
-		$this->assertEquals($result['action'], 'world');
-		$this->assertEquals($result['params'][0], 'value');
-		$this->assertEquals($result['params'][1], '7');
-	}
-	
-	public function testParseUriWithDirAndSeparatorNoParams() {
-		$c = new ConventionalController('dir', 'qw');
-		$result = $c->parseUri('hello/world');
-		
-		$this->assertEquals($result['className'], 'dir_Hello');
-		$this->assertEquals($result['action'], 'world');
-		$this->assertEmpty($result['params']);
-	}
-	
-	public function testParseUriWithDirAndSeparator() {
-		$c = new ConventionalController('dir', 'qw');
-		$result = $c->parseUri('hello/world/qw');
-		
-		$this->assertEquals($result['className'], 'dir_Hello');
-		$this->assertEquals($result['action'], 'world');
-		$this->assertEmpty($result['params']);
-	}
-	
-	public function testParseUriWithDirAndSeparatorAndParams() {
-		$c = new ConventionalController('dir', 'qw');
-		$result = $c->parseUri('hello/world/qw/value/7');
-		
-		$this->assertEquals($result['className'], 'dir_Hello');
-		$this->assertEquals($result['action'], 'world');
-		$this->assertEquals($result['params'][0], 'value');
-		$this->assertEquals($result['params'][1], '7');
 	}
 }
 
