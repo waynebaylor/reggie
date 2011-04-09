8c9b67eee1eb7831aff1d41b3f21ca99ef21f460
wtaylor <wtaylor@d7911c9c-ba0c-4a86-bf38-f0db2918b201>
2011-04-09 16:01:29 +0000

    added commands to build the js and include it in the package.

--- config/make_release.sh
+++ config/make_release.sh
@@ -4,12 +4,37 @@
 # usage: $> make_release.sh <release-name>
 #
 
+curr_dir=$(pwd)
+ 
 # create the tag for the release.
 #svn copy svn://dino/baylorsc/reggie/trunk svn://dino/baylorsc/reggie/tags/$1 -m "making another release"
 
-# check out the new tag.
+# check out the tag.
 svn checkout svn://dino/baylorsc/reggie/tags/$1 $1-tmp
 
+# build optimized js.
+mkdir $1-js
+mv $1-tmp/config/dojo-release-1.5.0-src.tar.gz $1-js
+tar -C $1-js -xvzf dojo-release-1.5.0-src.tar.gz
+cp $1-tmp/src/config/reggie.profile.js $1-js/dojo-release-1.5.0-src/util/buildscripts/profiles
+cd $1-js/dojo-release-1.5.0-src/util/buildscripts
+./build.sh profile=reggie
+cd $curr_dir
+
+# remove unoptimized js.
+rm -rf $1-tmp/src/public_html/js/dojo
+rm -rf $1-tmp/src/public_html/js/dojox
+rm -rf $1-tmp/src/public_html/js/dijit
+
+# copy optimized js into proj.
+cp -r $1-js/build-output/js/dojo $1-tmp/src/public_html/js
+cp -r $1-js/build-output/js/dojox $1-tmp/src/public_html/js
+cp -r $1-js/build-output/js/dijit $1-tmp/src/public_html/js
+
+# clean up js working dir.
+rm -rf $1-js
+
+# package up all the needed files.
 mv $1-tmp/src/public_html $1
 mv $1-tmp/config/.htaccess $1
 echo "$1" > $1/version.txt
