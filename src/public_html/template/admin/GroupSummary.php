27edca7545c125983765c6113e457c03bcccf28b
wtaylor <wtaylor@d7911c9c-ba0c-4a86-bf38-f0db2918b201>
2011-04-17 17:23:03 +0000

    added link to details page.

--- src/public_html/template/admin/GroupSummary.php
+++ src/public_html/template/admin/GroupSummary.php
@@ -29,7 +29,19 @@ class template_admin_GroupSummary extends template_AdminPage
 		return <<<_
 			<div id="content">
 				<h3>Group Summary</h3>
-
+				<div>
+					{$this->HTML->link(array(
+						'label' => 'Details',
+						'href' => '/admin/registration/Registration',
+						'parameters' => array(
+							'groupId' => $this->group['id'],
+							'reportId' => $this->report['id']
+						)
+					))}
+				</div>
+				
+				<div class="sub-divider"></div>
+				
 				{$f->html()}
 			</div>
 _;
